35b750e50341fa35adeae2b2926ff562393f425d
Eugene Korir <korir16071@gmail.com>
2024-10-15 13:13:56 +0300

    Initial commit terminal ui ratattui

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+use rat::App;
+use ratatui;
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::new().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
